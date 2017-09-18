8c79ff9f887244238c63467e770f837ea48c1b76
GeovaneDevelop <geovanefeitosacavalcante@gmail.com>
2017-09-18 20:36:25 -0300

    novo

--- public/js/Ajax/endereco.js
+++ public/js/Ajax/endereco.js
@@ -6,7 +6,7 @@
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("txtHint").innerHTML = this.responseText;
+                $("#txtHint").append(this.responseText);
                 document.getElementById('txtHint').disabled = false;
             }
         };
